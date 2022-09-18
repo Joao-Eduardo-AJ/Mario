09500bc9a22f2307b166d73566d7eaf2e0ff9cfd
João Eduardo Amorim Jardim <joaoeduaj@gmail.com>
2022-09-18 12:40:39 -0300

    Projeto em andamento

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
-const mario = document.querySelector('.mario')
+const mario = document.querySelector('.mario');
+const tubo = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
-
         mario.classList.remove('jump');
-
     }, 500);
 }
 
 const loop = setInterval(() => {
 
-    
+    const pipePosition = tubo.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (marioPosition < 80 && pipePosition <=  45 && pipePosition >=  0) {
+
+        tubo.style.animation = 'none';
+        tubo.style.left = `${pipePosition}px`;
+        tubo.style.bottom = 0;
+    }
 
 }, 10);
 
