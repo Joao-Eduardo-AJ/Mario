12d32f401568599c295de577b6e48be5c7c74a8d
João Eduardo Amorim Jardim <joaoeduaj@gmail.com>
2022-09-19 20:08:07 -0300

    aaa

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const mariodead = document.querySelector('.mario');
 const tubo = document.querySelector('.pipe');
 const grass = document.querySelector('#grass')
 const block = document.querySelector('#block')
@@ -17,9 +18,10 @@ const loop = setInterval(() => {
     const cloudPosition = cloud.offsetLeft;
     const pipePosition = tubo.offsetLeft;
     const blockPosition = block.offsetLeft;
+    const mariodeadPosition = mario.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (marioPosition < 30 && pipePosition <= 124 && pipePosition >=  80) {
+    if (marioPosition < 30 && pipePosition <= 129 && pipePosition >=  75) {
 
         cloud.style.animation = 'none';
         block.style.animation = 'none';
@@ -28,6 +30,7 @@ const loop = setInterval(() => {
         tubo.style.left = `${pipePosition}px`;
         block.style.left = `${blockPosition}px`;
         cloud.style.left = `${cloudPosition}px`;
+        mario.style.left = `${mariodeadPosition}px`;
         tubo.style.bottom = 0;
 
         mario.style.bottom = `${marioPosition}px`;
@@ -35,6 +38,10 @@ const loop = setInterval(() => {
         mario.classList.add('dead');
 
         clearInterval(loop);
+
+        setTimeout(() => {
+        document.location.reload(true)
+    }, 850)
 
     }
 }, 10);
