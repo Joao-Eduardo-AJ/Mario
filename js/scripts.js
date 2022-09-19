e00bf85565e309ceb56506bd42d84cd4409c7ffd
João Eduardo Amorim Jardim <joaoeduaj@gmail.com>
2022-09-19 19:06:23 -0300

    ta quase

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 const mario = document.querySelector('.mario');
 const tubo = document.querySelector('.pipe');
+const grass = document.querySelector('#grass')
+const block = document.querySelector('#block')
+const cloud = document.querySelector('#cloud')
 
 const jump = () => {
     mario.classList.add('jump');
@@ -11,13 +14,20 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
+    const cloudPosition = cloud.offsetLeft;
     const pipePosition = tubo.offsetLeft;
+    const blockPosition = block.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     if (marioPosition < 30 && pipePosition <= 124 && pipePosition >=  80) {
 
+        cloud.style.animation = 'none';
+        block.style.animation = 'none';
+        grass.style.animation = 'none';
         tubo.style.animation = 'none';
         tubo.style.left = `${pipePosition}px`;
+        block.style.left = `${blockPosition}px`;
+        cloud.style.left = `${cloudPosition}px`;
         tubo.style.bottom = 0;
 
         mario.style.bottom = `${marioPosition}px`;
