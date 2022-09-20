4cbcca04ec943851018e4b4c23d7ec54c4aa9ac7
João Eduardo Amorim Jardim <joaoeduaj@gmail.com>
2022-09-19 22:44:05 -0300

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -19,21 +19,22 @@ const loop = setInterval(() => {
     const pipePosition = tubo.offsetLeft;
     const blockPosition = block.offsetLeft;
     const mariodeadPosition = mario.offsetLeft;
+    const grassPosition = mario.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (marioPosition < 30 && pipePosition <= 129 && pipePosition >=  75) {
+    if (marioPosition < 330 && pipePosition <= 145 && pipePosition >=  97) {
 
         cloud.style.animation = 'none';
+        cloud.style.left = `${cloudPosition}px`;
         block.style.animation = 'none';
+        block.style.left = `${blockPosition}px`;
         grass.style.animation = 'none';
+        grass.style.left = `${grassPosition}px`;
         tubo.style.animation = 'none';
         tubo.style.left = `${pipePosition}px`;
-        block.style.left = `${blockPosition}px`;
-        cloud.style.left = `${cloudPosition}px`;
-        mario.style.left = `${mariodeadPosition}px`;
-        tubo.style.bottom = 0;
 
-        mario.style.bottom = `${marioPosition}px`;
+
+
         mario.src = './images/game-over.png';
         mario.classList.add('dead');
 
